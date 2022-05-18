6992cee2904b85e563d13af8040819b086ba1894
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2022-05-18 21:11:52 +0600

    Getx Dio

--- lib/View/Homepage/home_controller.dart
+++ lib/View/Homepage/home_controller.dart
@@ -9,6 +9,7 @@ class HomeController extends GetxController with StateMixin<List<PostModel>> {
   final Dio _dio = Dio();
   final RemoteService _remoteService = RemoteService (Dio());
 
+
   List<PostModel> dataList = <PostModel>[];
 
   @override
@@ -16,19 +17,17 @@ class HomeController extends GetxController with StateMixin<List<PostModel>> {
     getData();
     super.onInit();
   }
-
   Future<void> getData() async {
     change(null, status: RxStatus.loading());
-    await _remoteService.getAllPost().then(
-      (value) {
-        dataList = value;
-        change(dataList, status: RxStatus.success());
-      },
-      onError: (error) {
-        change(null, status: RxStatus.error(error.toString()));
-      },
-    );
+    try {
+      final res = await _remoteService.getAllPost();
+      change(res, status: RxStatus.success());
+    } catch (error) {
+      change(null, status: RxStatus.error(error.toString()));
+    }
   }
 
-
 }
+
+
+
